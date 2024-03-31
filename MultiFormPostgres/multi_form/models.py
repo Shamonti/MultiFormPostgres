5909e32a1ebef8c9b971884a69d143f47bfa615d
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-31 16:39:47 +0600

    Added html template

--- MultiFormPostgres/multi_form/models.py
+++ MultiFormPostgres/multi_form/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class FormData(models.Model):
   name = models.CharField(max_length = 100, default='')
-  phone = models.IntegerField(blank=True, null=True)
+  phone = models.IntegerField(blank=True, null=True, default=0)
   email = models.CharField(max_length = 100, default='')
   address = models.CharField(max_length=255, default='')  
   class Meta:
