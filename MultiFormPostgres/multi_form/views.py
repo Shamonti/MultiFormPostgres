568ea493e3ac62c0c945d1a443d759470f4cd40b
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-31 12:12:46 +0600

    Created the form and rendered form view

--- MultiFormPostgres/multi_form/views.py
+++ MultiFormPostgres/multi_form/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import FormDataForm
 
 # Create your views here.
+def render_form(request):
+  form = FormDataForm()
+  return render(request, 'form_template.html', {'form': form})
