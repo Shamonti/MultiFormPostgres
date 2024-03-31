ae21ed32e665c792cbd2c9e7656707b17325c804
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-31 12:31:18 +0600

    Added form submission view

--- MultiFormPostgres/multi_form/views.py
+++ MultiFormPostgres/multi_form/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormDataForm
 
 # Create your views here.
 def render_form(request):
   form = FormDataForm()
   return render(request, 'form_template.html', {'form': form})
+
+def submit_form(request):
+  if request.method == 'POST':
+    form = FormDataForm(request.POST)
+    return redirect('success_page')
+  else:
+    form = FormDataForm()
+    return render(request, 'form_template.html', {'form': form})
