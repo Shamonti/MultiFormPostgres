4dddd143e0a6ec96bd8e6234b1ff35e0ef9114ec
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-31 11:18:58 +0600

    Created the model

--- MultiFormPostgres/multi_form/models.py
+++ MultiFormPostgres/multi_form/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class FormData(models.Model):
+  name = models.CharField(max_length=100),
+  phone = models.CharField(max_length = 20),
+  email = models.EmailField(),
+  address = models.TextField()
+
+class Meta:
+  db_table = "Persons"
